f7950d15bdf64f985920184fa3d0aa55dd0d1c56
Cosme Escobedo <comeoes@gmail.com>
2018-02-23 20:51:43 -0700

    First real commit
    Created the first file of the repository. Its the base for the cldrive
    shell. it does nothing yet... Also, it catches the KeyboardInterupt
    exception.

---
+++ cldrive.py
@@ -0,0 +1,19 @@
+import os, sys
+
+def executeCommand(command):
+    
+
+def main()
+    while(True):
+        command = input("[ClDrive]> ")
+        executeCommand(command)
+
+
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print ('bye..')
+        sys.exit(0)
